15a45877e0646a2fd589ec35d232314e884010a4
Eric Otuvaka <eric.otuvaka@gmail.com>
2025-02-26 22:06:31 -0800

    first commit

---
+++ init.lua
@@ -0,0 +1,121 @@
+vim.g.base46_cache = vim.fn.stdpath "data" .. "/base46/"
+vim.g.mapleader = " "
+
+-------------------------------------------------------------------------------
+--
+-- preferences
+--
+-------------------------------------------------------------------------------
+-- never ever folding
+vim.opt.foldenable = false
+vim.opt.foldmethod = "manual"
+vim.opt.foldlevelstart = 99
+-- very basic "continue indent" mode (autoindent) is always on in neovim
+-- could try smartindent/cindent, but meh.
+-- vim.opt.cindent = true
+-- XXX
+-- vim.opt.cmdheight = 2
+-- vim.opt.completeopt = 'menuone,noinsert,noselect'
+-- not setting updatedtime because I use K to manually trigger hover effects
+-- and lowering it also changes how frequently files are written to swap.
+-- vim.opt.updatetime = 300
+-- if key combos seem to be "lagging"
+-- http://stackoverflow.com/questions/2158516/delay-before-o-opens-a-new-line
+-- vim.opt.timeoutlen = 300
+-- keep more context on screen while scrolling
+vim.opt.scrolloff = 2
+-- never show me line breaks if they're not there
+vim.opt.wrap = false
+-- always draw sign column. prevents buffer moving when adding/deleting sign
+vim.opt.signcolumn = "yes"
+-- sweet sweet relative line numbers
+vim.opt.relativenumber = true
+-- and show the absolute line number for the current line
+vim.opt.number = true
+-- keep current content top + left when splitting
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+-- infinite undo!
+-- NOTE: ends up in ~/.local/state/nvim/undo/
+vim.opt.undofile = true
+--" Decent wildmenu
+-- in completion, when there is more than one match,
+-- list all matches, and only complete to longest common match
+vim.opt.wildmode = "list:longest"
+-- when opening a file with a command (like :e),
+-- don't suggest files like there:
+vim.opt.wildignore = ".hg,.svn,*~,*.png,*.jpg,*.gif,*.min.js,*.swp,*.o,vendor,dist,_site"
+-- tabs: go big or go home
+vim.opt.shiftwidth = 8
+vim.opt.softtabstop = 8
+vim.opt.tabstop = 8
+vim.opt.expandtab = false
+-- case-insensitive search/replace
+vim.opt.ignorecase = true
+-- unless uppercase in search term
+vim.opt.smartcase = true
+-- never ever make my terminal beep
+vim.opt.vb = true
+-- more useful diffs (nvim -d)
+--- by ignoring whitespace
+vim.opt.diffopt:append "iwhite"
+--- and using a smarter algorithm
+--- https://vimways.org/2018/the-power-of-diff/
+--- https://stackoverflow.com/questions/32365271/whats-the-difference-between-git-diff-patience-and-git-diff-histogram
+--- https://luppeng.wordpress.com/2020/10/10/when-to-use-each-of-the-git-diff-algorithms/
+vim.opt.diffopt:append "algorithm:histogram"
+vim.opt.diffopt:append "indent-heuristic"
+-- show a column at 80 characters as a guide for long lines
+vim.opt.colorcolumn = "80"
+--- except in Rust where the rule is 100 characters
+vim.api.nvim_create_autocmd("Filetype", { pattern = "rust", command = "set colorcolumn=100" })
+-- show more hidden characters
+-- also, show tabs nicer
+vim.opt.listchars = "tab:^ ,nbsp:¬,extends:»,precedes:«,trail:•"
+
+-------------------------------------------------------------------------------
+--
+-- hotkeys
+--
+-------------------------------------------------------------------------------
+-- Neat X clipboard integration
+-- <leader>p will paste clipboard into buffer
+-- <leader>c will copy entire buffer into clipboard
+vim.keymap.set("n", "<leader>e", "<cmd>read !wl-paste<cr>")
+vim.keymap.set("n", "<leader>q", "<cmd>w !wl-copy<cr><cr>")
+
+-- bootstrap lazy and all plugins
+local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+
+if not vim.uv.fs_stat(lazypath) then
+    local repo = "https://github.com/folke/lazy.nvim.git"
+    vim.fn.system { "git", "clone", "--filter=blob:none", repo, "--branch=stable", lazypath }
+end
+
+vim.opt.rtp:prepend(lazypath)
+vim.o.background = "dark"
+
+local lazy_config = require "configs.lazy"
+
+-- load plugins
+require("lazy").setup({
+    {
+        "NvChad/NvChad",
+        lazy = false,
+        branch = "v2.5",
+        import = "nvchad.plugins",
+    },
+
+    { import = "plugins" },
+}, lazy_config)
+
+-- load theme
+dofile(vim.g.base46_cache .. "defaults")
+dofile(vim.g.base46_cache .. "statusline")
+
+require "options"
+require "nvchad.autocmds"
+
+vim.schedule(function()
+    require "mappings"
+end)
